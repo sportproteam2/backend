0ded70d75fa42a1dec880884a8ee176dceecbede
Azat <azat.kg.10@gmail.com>
2021-06-02 23:17:07 +0600

    Updated settings

--- sportpro/settings.py
+++ sportpro/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'sportpro.urls'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -137,7 +139,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
