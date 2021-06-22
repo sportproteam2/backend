d0ff4a334c88d0e2000f49b416c2ff9cf7cbd844
Azat <azat.kg.10@gmail.com>
2021-06-23 00:50:20 +0600

    fix: Updated req.txt

--- sportpro/settings.py
+++ sportpro/settings.py
@@ -54,12 +54,26 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000"
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://\w+\.api\.com$",
+]
+
+
 
 ROOT_URLCONF = 'sportpro.urls'
 
