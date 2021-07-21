81f586d8845f1214c5cfc2603d5a7b3199898b2c
Azat <azat.kg.10@gmail.com>
2021-07-21 19:53:08 +0600

    Fix: Deleted judge field from news:

--- sportpro_app/serializers.py
+++ sportpro_app/serializers.py
@@ -32,11 +32,10 @@ class NewsSerializer(serializers.ModelSerializer):
 
     author = UserSerializer(many=False)
     sport = SportSerializer(many=False)
-    judge = UserSerializer(many=False)
 
     class Meta:
         model = News
-        fields = ['id', 'title', 'article', 'author', 'judge', 'photo', 'dateofadd', 'sport', 'tags']
+        fields = ['id', 'title', 'article', 'author', 'photo', 'dateofadd', 'sport', 'tags']
 
     def create(self, validated_data):
         author = validated_data.pop('author')
